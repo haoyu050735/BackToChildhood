1fda7e59c648d67d4516928f957d60861d39fcfa
haoyu050735 <69667026+haoyu050735@users.noreply.github.com>
2021-04-17 01:58:46 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+function login() {
+var username = document.getElementById("username");
+if (username.value == "") {
+alert("請輸入關卡編號");
+} else if(username.value == "A1"){
+window.location.href="A1.html";
+} else if(username.value == "A2"){
+window.location.href="A2.html";
+} else if(username.value == "B1"){
+window.location.href="B1.html";
+} else if(username.value == "B2"){
+window.location.href="B2.html";
+} else if(username.value == "C1"){
+window.location.href="C1.html";
+} else if(username.value == "C2"){
+window.location.href="C2.html";
+} else {
+alert("請輸入正確的關卡編號！")
+}
+}
