a6f087afd61d2fb6ffc58c3e609f6374e43dd98e
haoyu050735 <69667026+haoyu050735@users.noreply.github.com>
2021-04-25 01:48:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+function login() {
+var username = document.getElementById("username");
+if (username.value == "") {
+alert("請輸入關卡編號");} 
+else if(username.value == "A1"){window.location.href="A1.html";} 
+else if(username.value == "A2"){window.location.href="A2.html";} 
+else if(username.value == "B1"){window.location.href="B1.html";} 
+else if(username.value == "B2"){window.location.href="B2.html";} 
+else if(username.value == "C1"){window.location.href="C1.html";} 
+else if(username.value == "C2"){window.location.href="C2.html";} 
+else if(username.value == "D1"){window.location.href="D1.html";} 
+else if(username.value == "D2"){window.location.href="D2.html";} 
+else if(username.value == "D3"){window.location.href="D3.html";} 
+else if(username.value == "A1-00"){window.location.href="A1-00.html";}
+else if(username.value == "A1-01"){window.location.href="A1-01.html";}
+else if(username.value == "A1-02"){window.location.href="A1-02.html";}
+else if(username.value == "A1-03"){window.location.href="A1-03.html";}
+else if(username.value == "A1-04"){window.location.href="A1-04.html";}
+else if(username.value == "A1-05"){window.location.href="A1-05.html";}
+else if(username.value == "A1-06"){window.location.href="A1-06.html";}
+else if(username.value == "A1-07"){window.location.href="A1-07.html";}
+else if(username.value == "A1-08"){window.location.href="A1-08.html";}
+else if(username.value == "A1-09"){window.location.href="A1-09.html";}
+else if(username.value == "A2-00"){window.location.href="A2-00.html";}
+else if(username.value == "A2-01"){window.location.href="A2-01.html";}
+else if(username.value == "A2-02"){window.location.href="A2-02.html";}
+else if(username.value == "A2-03"){window.location.href="A2-03.html";}
+else if(username.value == "A2-04"){window.location.href="A2-04.html";}
+else if(username.value == "A2-05"){window.location.href="A2-05.html";}
+else if(username.value == "A2-06"){window.location.href="A2-06.html";}
+else if(username.value == "A2-07"){window.location.href="A2-07.html";}
+else if(username.value == "A2-08"){window.location.href="A2-08.html";}
+else if(username.value == "A2-09"){window.location.href="A2-09.html";}
+else if(username.value == "B1-00"){window.location.href="B1-00.html";}
+else if(username.value == "B1-01"){window.location.href="B1-01.html";}
+else if(username.value == "B1-02"){window.location.href="B1-02.html";}
+else if(username.value == "B1-03"){window.location.href="B1-03.html";}
+else if(username.value == "B1-04"){window.location.href="B1-04.html";}
+else if(username.value == "B1-05"){window.location.href="B1-05.html";}
+else if(username.value == "B1-06"){window.location.href="B1-06.html";}
+else if(username.value == "B1-07"){window.location.href="B1-07.html";}
+else if(username.value == "B1-08"){window.location.href="B1-08.html";}
+else if(username.value == "B1-09"){window.location.href="B1-09.html";}
+else if(username.value == "B2-00"){window.location.href="B2-00.html";}
+else if(username.value == "B2-01"){window.location.href="B2-01.html";}
+else if(username.value == "B2-02"){window.location.href="B2-02.html";}
+else if(username.value == "B2-03"){window.location.href="B2-03.html";}
+else if(username.value == "B2-04"){window.location.href="B2-04.html";}
+else if(username.value == "B2-05"){window.location.href="B2-05.html";}
+else if(username.value == "B2-06"){window.location.href="B2-06.html";}
+else if(username.value == "B2-07"){window.location.href="B2-07.html";}
+else if(username.value == "B2-08"){window.location.href="B2-08.html";}
+else if(username.value == "B2-09"){window.location.href="B2-09.html";}
+else if(username.value == "C1-00"){window.location.href="C1-00.html";}
+else if(username.value == "C1-01"){window.location.href="C1-01.html";}
+else if(username.value == "C1-02"){window.location.href="C1-02.html";}
+else if(username.value == "C1-03"){window.location.href="C1-03.html";}
+else if(username.value == "C1-04"){window.location.href="C1-04.html";}
+else if(username.value == "C1-05"){window.location.href="C1-05.html";}
+else if(username.value == "C1-06"){window.location.href="C1-06.html";}
+else if(username.value == "C1-07"){window.location.href="C1-07.html";}
+else if(username.value == "C1-08"){window.location.href="C1-08.html";}
+else if(username.value == "C1-09"){window.location.href="C1-09.html";}
+else if(username.value == "C2-00"){window.location.href="C2-00.html";}
+else if(username.value == "C2-01"){window.location.href="C2-01.html";}
+else if(username.value == "C2-02"){window.location.href="C2-02.html";}
+else if(username.value == "C2-03"){window.location.href="C2-03.html";}
+else if(username.value == "C2-04"){window.location.href="C2-04.html";}
+else if(username.value == "C2-05"){window.location.href="C2-05.html";}
+else if(username.value == "C2-06"){window.location.href="C2-06.html";}
+else if(username.value == "C2-07"){window.location.href="C2-07.html";}
+else if(username.value == "C2-08"){window.location.href="C2-08.html";}
+else if(username.value == "C2-09"){window.location.href="C2-09.html";}
+else if(username.value == "D1-00"){window.location.href="D1-00.html";}
+else if(username.value == "D1-01"){window.location.href="D1-01.html";}
+else if(username.value == "D1-02"){window.location.href="D1-02.html";}
+else if(username.value == "D1-03"){window.location.href="D1-03.html";}
+else if(username.value == "D1-04"){window.location.href="D1-04.html";}
+else if(username.value == "D1-05"){window.location.href="D1-05.html";}
+else if(username.value == "D2-00"){window.location.href="D2-00.html";}
+else if(username.value == "D2-01"){window.location.href="D2-01.html";}
+else if(username.value == "D2-02"){window.location.href="D2-02.html";}
+else if(username.value == "D2-03"){window.location.href="D2-03.html";}
+else if(username.value == "D2-04"){window.location.href="D2-04.html";}
+else if(username.value == "D2-05"){window.location.href="D2-05.html";}
+else if(username.value == "D3-00"){window.location.href="D3-00.html";}
+else if(username.value == "D3-01"){window.location.href="D3-01.html";}
+else if(username.value == "D3-02"){window.location.href="D3-02.html";}
+else if(username.value == "D3-03"){window.location.href="D3-03.html";}
+else if(username.value == "D3-04"){window.location.href="D3-04.html";}
+else if(username.value == "D3-05"){window.location.href="D3-05.html";}
+
+else {alert("請輸入正確的關卡編號！")}
+}
+function find_npc() {window.location.href="find_npc.html";}
+function find_level() {window.location.href="find_level.html";}
+function find_token() {window.location.href="find_token.html";}
